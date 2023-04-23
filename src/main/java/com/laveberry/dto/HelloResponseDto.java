456d1656c961792a8af89b6c2015fa9270e9dc24
laveberry <laveberry7@gmail.com>
2023-04-23 16:14:41 +0900

    롬복

---
+++ src/main/java/com/laveberry/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.laveberry.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor //final 포함된 생성자 생성
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
--- src/test/java/com/laveberry/web/HelloControllerTest.java
+++ src/test/java/com/laveberry/web/HelloControllerTest.java
@@ -7,9 +7,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)//(1)RunWith : 스프링부트테스트와 JUnuit 연결자
 @WebMvcTest(controllers = HelloController.class)//(2)WebMvcTest : springMVC집중
@@ -24,5 +27,20 @@ public class HelloControllerTest {
         mvc.perform(get("/hello")) //(5)url Get 요청
                 .andExpect(status().isOk())  //(6)(7)Http header status(200) 결과검증
                 .andExpect(content().string(hello)); //(8)응답본문내용 검증
+    }
+
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name) //1.param : API호출시 요청 파라미터. String만 가능
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))//2.jsonPath :json응답값 필드별($.필드명) 검증
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
