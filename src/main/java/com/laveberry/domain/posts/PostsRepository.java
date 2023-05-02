02292f9cecd81d18e543a8d2e77f049daabf3ce1
laveberry <laveberry7@gmail.com>
2023-05-03 06:52:11 +0900

    머스테치로 화면 구성 - 전체조회 화면

--- src/main/java/com/laveberry/domain/posts/PostsRepository.java
+++ src/main/java/com/laveberry/domain/posts/PostsRepository.java
@@ -1,7 +1,15 @@
 package com.laveberry.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 //JpaRepository<Entity클래스, PK타입> : DB접근하게 해줌
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    //쿼리 직접 작성도 가능
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
+
 }
--- src/main/java/com/laveberry/service/posts/PostsService.java
+++ src/main/java/com/laveberry/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.laveberry.service.posts;
 
 import com.laveberry.domain.posts.Posts;
 import com.laveberry.domain.posts.PostsRepository;
+import com.laveberry.web.dto.PostsListResponseDto;
 import com.laveberry.web.dto.PostsResponseDto;
 import com.laveberry.web.dto.PostsSaveRequestDto;
 import com.laveberry.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor //생성자로 Bean주입받기. @Autowired와 동일효과
 @Service
@@ -34,5 +38,12 @@ public class PostsService {
                 .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) //조회기능 트랜잭션 범위 유지
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) //map(posts -> new PostsListResponseDto(Posts))
+                .collect(Collectors.toList());
     }
 }
